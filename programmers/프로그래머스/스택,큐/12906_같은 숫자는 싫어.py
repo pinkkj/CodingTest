a96e9b92537be4fa31b3d30de1c4f287247162e4
pinkkj <juventa23@daum.net>
2025-08-03 10:48:29 +0900

    5m

---
+++ programmers/프로그래머스/스택,큐/12906_같은 숫자는 싫어.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    answer.append(arr[0])
+    for i in arr[1:]:
+        if answer[-1] == i:
+            continue
+        else:
+            answer.append(i)
+        
+    return answer
